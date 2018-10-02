831acfc4aca60e5f821b33ce70382cf8a336a27e
Dattatray Kale <way2datta@gmail.com>
2018-10-02 16:11:30 +0530

    Bootstrap, navigation and router, dashboard

--- angular-webapi/BeingDK.ExpenseTracker.Web.SpaWithWebApi/src/app/app.module.ts
+++ angular-webapi/BeingDK.ExpenseTracker.Web.SpaWithWebApi/src/app/app.module.ts
@@ -1,15 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { ExpensesComponent } from '../app/expenses/expenses.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'expenses',
+    component: ExpensesComponent,
+    data: { title: 'Expenses' }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    data: { title: 'Dashboard' }
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ExpensesComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
