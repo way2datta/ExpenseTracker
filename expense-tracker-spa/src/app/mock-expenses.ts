6197ecc32fa568fae28c3a14dc9726427cb7adec
Dattatray Kale <way2datta@gmail.com>
2018-10-22 20:04:30 +0530

    Removed files

--- expense-tracker-spa/src/app/mock-expenses.ts
+++
@@ -1,7 +0,0 @@
-import { Expense } from './expense';
-
-export const EXPENSES: Expense[] = [
-  { id: 1, name: 'Bills' },
-  { id: 2, name: 'Grocery' },
-  { id: 3, name: 'Food' }
-];
--- mvc-api/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvc_api.MvcApplication" Language="C#" %>
